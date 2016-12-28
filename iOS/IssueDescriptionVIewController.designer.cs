7e88a09dfe4676fb2a154cd15d02107a95dec0f1
Antonio Feregrino <antonio.feregrino@gmail.com>
2016-12-27 23:38:01 -0600

    Issue detail view

--- iOS/IssueDescriptionVIewController.designer.cs
+++ iOS/IssueDescriptionVIewController.designer.cs
@@ -12,9 +12,39 @@ namespace MyIssues2.iOS
 	[Register ("IssueDescriptionVIewController")]
 	partial class IssueDescriptionVIewController
 	{
+		[Outlet]
+		UIKit.UILabel IssueDetailLabel { get; set; }
+
+		[Outlet]
+		UIKit.UILabel IssueStatusLabel { get; set; }
+
+		[Outlet]
+		UIKit.UIView IssueStatusView { get; set; }
+
+		[Outlet]
+		UIKit.UILabel IssueTitleLabel { get; set; }
 		
 		void ReleaseDesignerOutlets ()
 		{
+			if (IssueTitleLabel != null) {
+				IssueTitleLabel.Dispose ();
+				IssueTitleLabel = null;
+			}
+
+			if (IssueStatusView != null) {
+				IssueStatusView.Dispose ();
+				IssueStatusView = null;
+			}
+
+			if (IssueStatusLabel != null) {
+				IssueStatusLabel.Dispose ();
+				IssueStatusLabel = null;
+			}
+
+			if (IssueDetailLabel != null) {
+				IssueDetailLabel.Dispose ();
+				IssueDetailLabel = null;
+			}
 		}
 	}
 }
